81c0788624643f0c95e385261bd2b86f3210a996
ibrahim <nmibrahim91@gmail.com>
2026-02-13 12:33:38 +0200

    midleware

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ import express from 'express'
 import dotenv from 'dotenv'
 import mysql from 'mysql'
 import bp from 'body-parser'
-import bycrybtjs from 'bcryptjs'
-// import jwt from 'jsonwebtoken'
+import bycryptjs from 'bcryptjs'
+import jwt from 'jsonwebtoken'
 
 dotenv.config()
 
@@ -20,9 +20,8 @@ const mydb = mysql.createConnection({
 
 mydb.connect(function(err){
     if(err) throw new Error(err)
-        console.log('Mysql Databse connected')
+    console.log('Mysql Databse connected')
 })
-
 
 app.get('/',function(req,res){
     try{
@@ -52,6 +51,53 @@ app.post("/register",function(req,res){
    
 })
 
+app.post("/login", function (req, res) {
+    const { email, password } = req.body;
+
+    mydb.query(
+        "SELECT * FROM users WHERE email = ?",
+        [email],
+        async (err, results) => {
+            if (err) return res.status(500).json({ message: "DB error" });
+
+            if (results.length === 0) {
+                return res.status(404).json({ message: "user not found" });
+            }
+
+            const user = results[0];
+
+            const matchedPassword = await bycryptjs.compare(password, user.password);
+
+            if (!matchedPassword) {
+                return res.status(401).json({ message: "invalid credentials" });
+            }
+
+            const token = jwt.sign(
+                { id: user.id, email: user.email },
+                process.env.JWT_SECRET,
+                { expiresIn: "1h" }
+            );
+
+            res.status(200).json({message: "login successful",token});
+        }
+    );
+});
+
+//middleware to verify token
+
+const verifytoken=(req,res,next)=>{
+    const authheader =req.headers['authorization']
+    if(!authheader) return res.status(401).json({message:'no token provided'})
+
+    const token = authheader.split(' ')[1]
+    if(!token) return response.status(401).json({message:'no token provided'})
+
+    jwt.verify(token,process.env.JWT_SECRET,(err,decoded)=>{
+        if(err) return res.status(401).json({message:'invalid token'})
+        req.user = decoded //attach our user to a request
+    next()//execute our real API function after these checks//allow request
+    })
+}
 
 // GET all users(students)
 
@@ -71,16 +117,7 @@ app.get('/users/:id', function(req,res){
     })
 
 })
-
-// app.post('/users',function(req,res){
-//     const {id,username,email,password} = req.body
-//     mydb.query('insert into users (id,username,email,password) values(?,?,?,?)',[id,username,email,password],(err,results)=>{
-//         if(err) throw new Error(err)
-//         res.json({message: 'Added a user',results})
-//     })
-    
-// })
-app.put('/users/:id',function(req,res){
+app.put('/users/:id',verifytoken,function(req,res){
     const id = req.params.id;
     const {username,email,password} = req.body
     mydb.query('UPDATE users SET username=?,email=?,password=? WHERE id =?'
